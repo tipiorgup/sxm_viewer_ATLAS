0755f75d28dd87283fdf80b4e909e32cf1b18db8
Orgup <51993842+tipiorgup@users.noreply.github.com>
2026-05-19 12:54:23 +0200

    correct grid

--- sxm_viewer/gui/dialogs/position_coordinates_dialogs.py
+++ sxm_viewer/gui/dialogs/position_coordinates_dialogs.py
@@ -261,7 +261,7 @@ class PositionCoordinatesDialog(QtWidgets.QDialog):
 
         # Corrugation: subtract background (minimum), then convert to Å
         z = (arr - np.nanmin(arr)) * factor
-        if ymin > ymax:
+        if ymin < ymax:
             z = np.flipud(z)
         if xmin > xmax:
             z = np.fliplr(z)
